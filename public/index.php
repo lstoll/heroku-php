6225067d1c6d9528466c2b7bf2f7f394fb1b5aac
Lincoln Stoll <lstoll@lstoll.net>
2011-08-30 15:36:49 -0700

    Beginnings of sprucing up a home page

--- public/index.php
+++ public/index.php
@@ -1,3 +1,14 @@
-<?php
-echo "Hello, World! - from PHP.";
-?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel="stylesheet" href="http://twitter.github.com/bootstrap/assets/css/bootstrap-1.1.1.min.css">
+<body>
+<div class="container">
+    <h1>PHP On Heroku</h1>
+    This is a PHP page being served on heroku
+    <?php
+        echo phpinfo();
+    ?>
+</div>
+</body>
+</html>
