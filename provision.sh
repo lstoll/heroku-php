1424e43c89b06b905b6f326c59cdb6720512e4f5
Lincoln Stoll <lstoll@lstoll.net>
2011-08-30 15:21:02 -0700

    Build php 5.4 automatically.

---
+++ provision.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get -y install build-essential libxml2-dev libpq-dev
+wget http://snaps.php.net/php5.4-latest.tar.bz2
+tar -jxvf php5.4-latest.tar.bz2
+cd php5.4*
+./configure --with-pgsql --prefix=/app/sw
+make
+make install
